91d2a984199809faaff8f6277b66faaeb7fd56f5
Pablo Correa <pablocorrea523@gmail.com>
2022-11-06 17:45:23 -0300

    first commit

---
+++ src/components/Cards/index.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+
+const Cards = () => {
+  const key =
+    "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjZhMTY1MDgwLTA1ZDEtNDQyZS1iZTUwLWEzNmY3NGE1YzdkNiIsImlhdCI6MTY2Nzc2MTAzOCwic3ViIjoiZGV2ZWxvcGVyL2I3ZmNlMmVlLWMzMzctMTRiMC01NzdiLTU5OWMyMTExMzJhZCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIyMDcuMjQ4LjIxNC4xOTQiXSwidHlwZSI6ImNsaWVudCJ9XX0.pTENxI907TOLXs-tut5PrwxSnwzMlEQRFkVRhRtFKrtK_zETf1F4UFxabjFPqorxFGqgkHnPgm4XoTodD6875Q";
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization:
+      "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjZhMTY1MDgwLTA1ZDEtNDQyZS1iZTUwLWEzNmY3NGE1YzdkNiIsImlhdCI6MTY2Nzc2MTAzOCwic3ViIjoiZGV2ZWxvcGVyL2I3ZmNlMmVlLWMzMzctMTRiMC01NzdiLTU5OWMyMTExMzJhZCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIyMDcuMjQ4LjIxNC4xOTQiXSwidHlwZSI6ImNsaWVudCJ9XX0.pTENxI907TOLXs-tut5PrwxSnwzMlEQRFkVRhRtFKrtK_zETf1F4UFxabjFPqorxFGqgkHnPgm4XoTodD6875Q",
+    Accept: "*/*",
+  };
+
+  const getData = async () => {
+    const url = `https://api.clashroyale.com/v1/cards?limit=200`;
+    const request = fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjZhMTY1MDgwLTA1ZDEtNDQyZS1iZTUwLWEzNmY3NGE1YzdkNiIsImlhdCI6MTY2Nzc2MTAzOCwic3ViIjoiZGV2ZWxvcGVyL2I3ZmNlMmVlLWMzMzctMTRiMC01NzdiLTU5OWMyMTExMzJhZCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIyMDcuMjQ4LjIxNC4xOTQiXSwidHlwZSI6ImNsaWVudCJ9XX0.pTENxI907TOLXs-tut5PrwxSnwzMlEQRFkVRhRtFKrtK_zETf1F4UFxabjFPqorxFGqgkHnPgm4XoTodD6875Q",
+        Accept: "*/*",
+      },
+    });
+    const response = (await request).json();
+    console.log(response);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return <div></div>;
+};
+
+export default Cards;
