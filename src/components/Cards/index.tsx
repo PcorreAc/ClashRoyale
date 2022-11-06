217ca3fcad9d522c6923a1538f0879544c34a866
Pablo Correa <pablocorrea523@gmail.com>
2022-11-06 18:07:26 -0300

    second test

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -1,29 +1,28 @@
 import React, { useEffect, useState } from "react";
+import { json } from "react-router-dom";
 
 const Cards = () => {
-  const [data, setData] = useState([]);
+  const [cards, setCards] = useState([]);
   const key =
     "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjZhMTY1MDgwLTA1ZDEtNDQyZS1iZTUwLWEzNmY3NGE1YzdkNiIsImlhdCI6MTY2Nzc2MTAzOCwic3ViIjoiZGV2ZWxvcGVyL2I3ZmNlMmVlLWMzMzctMTRiMC01NzdiLTU5OWMyMTExMzJhZCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIyMDcuMjQ4LjIxNC4xOTQiXSwidHlwZSI6ImNsaWVudCJ9XX0.pTENxI907TOLXs-tut5PrwxSnwzMlEQRFkVRhRtFKrtK_zETf1F4UFxabjFPqorxFGqgkHnPgm4XoTodD6875Q";
   const headers = {
     "Content-Type": "application/json",
-    Authorization:
-      "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjZhMTY1MDgwLTA1ZDEtNDQyZS1iZTUwLWEzNmY3NGE1YzdkNiIsImlhdCI6MTY2Nzc2MTAzOCwic3ViIjoiZGV2ZWxvcGVyL2I3ZmNlMmVlLWMzMzctMTRiMC01NzdiLTU5OWMyMTExMzJhZCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIyMDcuMjQ4LjIxNC4xOTQiXSwidHlwZSI6ImNsaWVudCJ9XX0.pTENxI907TOLXs-tut5PrwxSnwzMlEQRFkVRhRtFKrtK_zETf1F4UFxabjFPqorxFGqgkHnPgm4XoTodD6875Q",
+    Authorization: "Bearer " + key,
     Accept: "*/*",
   };
 
   const getData = async () => {
     const url = `https://api.clashroyale.com/v1/cards?limit=200`;
-    const request = fetch(url, {
+    const response = await fetch(url, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        Authorization:
-          "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjZhMTY1MDgwLTA1ZDEtNDQyZS1iZTUwLWEzNmY3NGE1YzdkNiIsImlhdCI6MTY2Nzc2MTAzOCwic3ViIjoiZGV2ZWxvcGVyL2I3ZmNlMmVlLWMzMzctMTRiMC01NzdiLTU5OWMyMTExMzJhZCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIyMDcuMjQ4LjIxNC4xOTQiXSwidHlwZSI6ImNsaWVudCJ9XX0.pTENxI907TOLXs-tut5PrwxSnwzMlEQRFkVRhRtFKrtK_zETf1F4UFxabjFPqorxFGqgkHnPgm4XoTodD6875Q",
+        Authorization: "Bearer " + key,
         Accept: "*/*",
       },
     });
-    const response = (await request).json();
-    setData(await response);
+    const data = await response.json();
+    setCards(data);
   };
 
   useEffect(() => {
@@ -31,7 +30,7 @@ const Cards = () => {
   }, []);
   return (
     <div>
-      {data.map((carta: any) => (
+      {cards.map((carta: any) => (
         <p>{carta.name}</p>
       ))}
     </div>
