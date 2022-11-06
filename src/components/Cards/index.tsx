b2946d9fa4c4a4f2260b01478ad4ac0fca1d7e81
Pablo Correa <pablocorrea523@gmail.com>
2022-11-06 17:56:24 -0300

    test

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Cards = () => {
+  const [data, setData] = useState([]);
   const key =
     "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjZhMTY1MDgwLTA1ZDEtNDQyZS1iZTUwLWEzNmY3NGE1YzdkNiIsImlhdCI6MTY2Nzc2MTAzOCwic3ViIjoiZGV2ZWxvcGVyL2I3ZmNlMmVlLWMzMzctMTRiMC01NzdiLTU5OWMyMTExMzJhZCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIyMDcuMjQ4LjIxNC4xOTQiXSwidHlwZSI6ImNsaWVudCJ9XX0.pTENxI907TOLXs-tut5PrwxSnwzMlEQRFkVRhRtFKrtK_zETf1F4UFxabjFPqorxFGqgkHnPgm4XoTodD6875Q";
   const headers = {
@@ -22,13 +23,19 @@ const Cards = () => {
       },
     });
     const response = (await request).json();
-    console.log(response);
+    setData(await response);
   };
 
   useEffect(() => {
     getData();
   }, []);
-  return <div></div>;
+  return (
+    <div>
+      {data.map((carta: any) => (
+        <p>{carta.name}</p>
+      ))}
+    </div>
+  );
 };
 
 export default Cards;
